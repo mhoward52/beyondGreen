e51d2743e9323957659c20e5e8c988189a34cb1a
Michael Howard <michael.howard.pha@icloud.com>
2019-06-18 11:30:37 -0500

    Added Customer model

--- models/customer.js
+++ models/customer.js
@@ -1,3 +1,58 @@
 const mongoose =require('mongoose');
 const Joi = require('joi');
 
+const Customer = mongoose.model('Customer', new mongoose.Schema({
+    companyName: {
+        type: String,
+        required: true
+    },
+
+    address: {
+        type: String,
+        required: true
+    },
+
+    city: {
+        type: String,
+        required: true
+    },
+
+    state: {
+        type: String,
+        required: true,
+        enum: [
+            "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "District Of Columbia","Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"
+        ]
+    },
+
+    zipCode: {
+        type: Number,
+        required: true,
+        minlength: 5
+    },
+
+    deliveryCharge: {
+        type: Number
+    },
+
+    sackLunchPrice: {
+        type: Number
+    }
+
+}));
+
+function validateCustomer(customer) {
+    const schema = {
+        companyName: Joi.string().required(),
+        address: Joi.string().required(),
+        city: Joi.string().required(),
+        state: Joi.string().required(),
+        zipCode: Joi.string().min(5).required(),
+        deliveryCharge: Joi.number(),
+        sackLunchPrice: Joi.number()
+    };
+    return Joi.validate(customer, schema);
+}
+
+exports.Customer = Customer;
+exports.validate = validateCustomer;
